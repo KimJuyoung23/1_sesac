8f1ab4c7605a1b9223e65b701a25d2e39971f8cf
kjy <nashirah23@gmail.com>
2023-10-05 16:06:58 +0900

    231005 matplot iris

---
+++ 0918_1020/1005_iris_1.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+import numpy as np
+
+figsize = (10, 10)
+iris = load_iris()
+
+feature_names = iris.feature_names # 특징 
+n_feature = len(feature_names) # 4
+species = iris.target_names # 종
+n_species = len(species) # 3
+iris_X, iris_y = iris.data, iris.target # 데이터, 종
+iris_X = np.transpose(iris_X) # transpose
+data = iris_X.reshape(n_feature, n_species, -1) # (특징, 종, 데이터)
+
+list_n_species = [i for i in range(n_species)] # xposition, xticks 위한 리스트
+
+fig, axes = \
+    plt.subplots(2, 2, figsize = figsize) # 2 * 2 subplots 
+for ax_idx, ax in enumerate(axes.flat):
+    ax.violinplot([data[ax_idx][i] for i in list_n_species], positions=list_n_species) # 
+    ax.set_title(feature_names[ax_idx], fontsize=10)
+    ax.set_xticks(list_n_species)
+    ax.set_xticklabels(species)
+    ax.tick_params(labelsize=10)
+    ax.grid()
+
+plt.show()
---
+++ 0918_1020/1005_iris_2.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+import numpy as np
+
+figsize = (10, 10)
+iris = load_iris()
+iris_X, iris_y = iris.data, iris.target
+feature_names = iris.feature_names
+n_feature = len(feature_names)
+
+fig, axes = plt.subplots(4, 4, figsize=figsize)
+
+for i, ax in enumerate(axes.flat):
+    row_feat, col_feat = i // n_feature, i % n_feature
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    if row_feat == 0:
+        ax.set_ylabel(feature_names[col_feat], fontsize=13)
+    if col_feat == n_feature-1:
+        ax.set_xlabel(feature_names[row_feat], fontsize=13)
+
+    if row_feat == col_feat:
+        ax.hist(iris_X[:, row_feat], rwidth=0.9)
+    else:
+        ax.scatter(iris_X[:, row_feat], iris_X[:, col_feat], c=iris_y, alpha=0.5)
+        ax.grid()
+
+plt.tight_layout()
+plt.show()
