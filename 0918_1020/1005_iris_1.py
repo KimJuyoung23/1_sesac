23c4db89dfdd7eb2b25e3b2ea9e7f12df3558bb9
kjy <nashirah23@gmail.com>
2023-10-06 10:06:49 +0900

    231005 matplot iris

--- 0918_1020/1005_iris_1.py
+++ 0918_1020/1005_iris_1.py
@@ -5,11 +5,11 @@ import numpy as np
 figsize = (10, 10)
 iris = load_iris()
 
-feature_names = iris.feature_names # 특징 
+feature_names = iris.feature_names # 특징  # type: ignore
 n_feature = len(feature_names) # 4
-species = iris.target_names # 종
+species = iris.target_names # 종 # type: ignore
 n_species = len(species) # 3
-iris_X, iris_y = iris.data, iris.target # 데이터, 종
+iris_X, iris_y = iris.data, iris.target # 데이터, 종 # type: ignore
 iris_X = np.transpose(iris_X) # transpose
 data = iris_X.reshape(n_feature, n_species, -1) # (특징, 종, 데이터)
 
@@ -18,11 +18,11 @@ list_n_species = [i for i in range(n_species)] # xposition, xticks 위한 리스
 fig, axes = \
     plt.subplots(2, 2, figsize = figsize) # 2 * 2 subplots 
 for ax_idx, ax in enumerate(axes.flat):
-    ax.violinplot([data[ax_idx][i] for i in list_n_species], positions=list_n_species) # 
-    ax.set_title(feature_names[ax_idx], fontsize=10)
-    ax.set_xticks(list_n_species)
-    ax.set_xticklabels(species)
-    ax.tick_params(labelsize=10)
-    ax.grid()
+    ax.violinplot([data[ax_idx][i] for i in list_n_species], positions=list_n_species) # type: ignore # 
+    ax.set_title(feature_names[ax_idx], fontsize=10) # type: ignore
+    ax.set_xticks(list_n_species) # type: ignore
+    ax.set_xticklabels(species) # type: ignore
+    ax.tick_params(labelsize=10) # type: ignore
+    ax.grid() # type: ignore
 
 plt.show()
--- 0918_1020/1005_iris_2.py
+++ 0918_1020/1005_iris_2.py
@@ -4,8 +4,8 @@ import numpy as np
 
 figsize = (10, 10)
 iris = load_iris()
-iris_X, iris_y = iris.data, iris.target
-feature_names = iris.feature_names
+iris_X, iris_y = iris.data, iris.target # type: ignore
+feature_names = iris.feature_names # type: ignore
 n_feature = len(feature_names)
 
 fig, axes = plt.subplots(4, 4, figsize=figsize)
@@ -13,19 +13,19 @@ fig, axes = plt.subplots(4, 4, figsize=figsize)
 for i, ax in enumerate(axes.flat):
     row_feat, col_feat = i // n_feature, i % n_feature
 
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False) # type: ignore
+    ax.spines['right'].set_visible(False) # type: ignore
 
     if row_feat == 0:
-        ax.set_ylabel(feature_names[col_feat], fontsize=13)
+        ax.set_ylabel(feature_names[col_feat], fontsize=13) # type: ignore
     if col_feat == n_feature-1:
-        ax.set_xlabel(feature_names[row_feat], fontsize=13)
+        ax.set_xlabel(feature_names[row_feat], fontsize=13) # type: ignore
 
     if row_feat == col_feat:
-        ax.hist(iris_X[:, row_feat], rwidth=0.9)
+        ax.hist(iris_X[:, row_feat], rwidth=0.9) # type: ignore
     else:
-        ax.scatter(iris_X[:, row_feat], iris_X[:, col_feat], c=iris_y, alpha=0.5)
-        ax.grid()
+        ax.scatter(iris_X[:, row_feat], iris_X[:, col_feat], c=iris_y, alpha=0.5) # type: ignore
+        ax.grid() # type: ignore
 
 plt.tight_layout()
 plt.show()
